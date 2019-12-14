e16cd3a8bc9cb3d1e5d31ddbee23fb713739d510
Said Id-bella ali <sid-bell@e1r5p1.1337.ma>
2019-12-14 18:33:35 +0100

    ok

--- src/includes/shell.h
+++ src/includes/shell.h
@@ -6,7 +6,7 @@
 /*   By: sid-bell <sid-bell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/18 15:40:36 by sid-bell          #+#    #+#             */
-/*   Updated: 2019/12/04 13:15:18 by mmostafa         ###   ########.fr       */
+/*   Updated: 2019/12/14 18:31:03 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,15 @@
 # include "libft.h"
 # include "builtins.h"
 # include "jobcontrol.h"
-
+# include <utmpx.h>
+# include <sys/mman.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <unistd.h>
 # define EXIT_ON_EOT 0
 # define RETURN_ON_EOT 1
 
+char	    *ft_getusername(void);
 t_shell		*get_shell_cfg(t_shell *new);
 void		init_(t_shell *shell, char **env, t_init *init);
 #endif
--- src/init.c
+++ src/init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yelazrak <yelazrak@student.42.fr>          +#+  +:+       +#+        */
+/*   By: sid-bell <sid-bell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/28 12:12:08 by sid-bell          #+#    #+#             */
-/*   Updated: 2019/12/14 14:39:17 by yelazrak         ###   ########.fr       */
+/*   Updated: 2019/12/14 18:32:48 by sid-bell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,28 @@ t_shell	*get_shell_cfg(t_shell *new)
 	if (new)
 		sh = new;
 	return (sh);
+}
+
+char	*ft_getusername(void)
+{
+	int				fd;
+	struct stat		st;
+	void			*ptr;
+	off_t			i;
+	struct utmpx	*ut;
+
+	fd = open("/var/run/utmpx", O_RDONLY);
+	fstat(fd, &st);
+	ptr = mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+	i = 0;
+	while (i < st.st_size)
+	{
+		ut = ptr+i;
+		if (ut->ut_type == USER_PROCESS)
+			return (ut->ut_user);
+		i += 628;
+	}
+	return (NULL);
 }
 
 void	init_(t_shell *shell, char **env, t_init *init)
