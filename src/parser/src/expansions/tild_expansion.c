41373d97714cd1306b3d17cbd75c701462820ff3
Mamouni Mostafa <mmostafa@e1r4p2.1337.ma>
2019-12-19 11:46:20 +0100

    fix tild bug

--- src/parser/src/expansions/tild_expansion.c
+++ src/parser/src/expansions/tild_expansion.c
@@ -6,7 +6,7 @@
 /*   By: yoyassin <yoyassin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/04 19:30:27 by mmostafa          #+#    #+#             */
-/*   Updated: 2019/12/06 13:17:21 by yoyassin         ###   ########.fr       */
+/*   Updated: 2019/12/19 11:45:00 by mmostafa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,11 @@ char	*tild_expan(char *tild_str)
 	if (!ft_strcmp(tild_str, "~"))
 	{
 		if (!(home_dir = ft_getenv("HOME")))
-			home_dir = getpwuid(getuid())->pw_name;
+			home_dir = getpwnam(ft_getusername())->pw_dir;
 		return (ft_strdup(home_dir));
 	}
 	user_name = ft_strsub(tild_str, 1, ft_strlen(tild_str) - 1);
 	if (!(pw = getpwnam(user_name)))
 		return (ft_strdup(tild_str));
-	return (pw->pw_name);
+	return (pw->pw_dir);
 }
